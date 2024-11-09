bf85755fa09e6db8b00938f8d0baa187a7fe5cf6
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-09 21:33:07 +0100

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Jean on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
