7ac41fda4f033f917dd8ad2d0828f41e77d023ef
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-10 21:50:24 +0100

    164. Seeding Tags into the Database

--- Budget/BudgetApp.swift
+++ Budget/BudgetApp.swift
@@ -11,13 +11,29 @@ import SwiftUI
 struct BudgetApp: App {
     
     let provider: CoreDataProvider
+    let tagsSeeder: TagsSeeder
     
     init() {
         provider = CoreDataProvider()
+        tagsSeeder = TagsSeeder(context: provider.context)
+        
     }
     var body: some Scene {
         WindowGroup {
             BudgetListScreen()
+                .onAppear{
+                    let hasSeededData = UserDefaults.standard.bool(forKey: "hasSeedData")
+                    if !hasSeededData {
+                        let commonTags = ["Food", "Dining", "Travel", "Entertainment", "Shopping", "Health", "Utilities", "Transportation", "Other"]
+                        do {
+                            try tagsSeeder.seed( commonTags)
+                            UserDefaults.standard.setValue(true, forKey: "hasSeedData")
+                        } catch {
+                            print("Error seeding tags: \(error)")
+                        }
+                                 }
+                    
+                }
                 .environment(\.managedObjectContext, provider.context)
         }
     }
---
+++ Budget/Seeders/TagsSeeder.swift
@@ -0,0 +1,31 @@
+//
+//  TagsSeeder.swift
+//  Budget
+//
+//  Created by Jean on 10/11/24.
+//
+
+import Foundation
+import CoreData
+
+class TagsSeeder {
+    
+    private var context: NSManagedObjectContext
+    
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    func seed( _ commonTags: [String]) throws
+    {
+        
+        for commonTag in commonTags {
+            let tag = Tag(context: context)
+            
+            tag.name = commonTag
+            
+            try context.save()
+        }
+    }
+}
