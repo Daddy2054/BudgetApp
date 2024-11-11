8c2d122578608db1b4687e8abb49ed4d0b3156ac
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-11 23:15:58 +0100

    175. Navigating to Filter Screen, Total Budget and Adding ContentUnavailableView

--- Budget/Screens/BudgetListScreen.swift
+++ Budget/Screens/BudgetListScreen.swift
@@ -10,31 +10,68 @@ import SwiftUI
 struct BudgetListScreen: View {
     @FetchRequest(sortDescriptors: []) private var budgets: FetchedResults<Budget>
     @State private var isPresented: Bool = false
+    @State private var isFilterPresented: Bool = false
+    
+    private var total: Double {
+        budgets.reduce(0) { $0 + $1.limit }
+    }
     
     var body: some View {
         VStack {
-            List(budgets) { budget in
-                NavigationLink {
-                    BudgetDetailScreen(budget: budget)} label: {
-                        BudgetCellView(budget: budget)
+            
+            if budgets.isEmpty {
+                ContentUnavailableView("No budget yet",systemImage: "list.clipboard")
+            } else  {
+                
+                List {
+                    HStack {
+                        Spacer()
+                        Text("Total Limit")
+                        
+                        Text(total,format: .currency(code:Locale.currencyCode))
+                        Spacer()
                     }
-            }
-        }.navigationTitle("Budget App")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing){
-                    Button("Add Budget") {
-                        isPresented = true
+                    .font(.headline)
+                    
+                    ForEach(budgets) { budget in
+                        NavigationLink {
+                            BudgetDetailScreen(budget: budget)} label: {
+                                BudgetCellView(budget: budget)
+                            }
                     }
                 }
-            } .sheet(isPresented: $isPresented, content: {
-                AddBudgetScreen()
-            } )  }
+            }
+        }
+        .overlay(alignment: .bottom, content: {
+            Button("Filter"){
+                isFilterPresented = true
+            }.buttonStyle(.borderedProminent)
+                .tint(.gray)
+        }
+        )
+        .navigationTitle("Budget App")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing){
+                Button("Add Budget") {
+                    isPresented = true
+                }
+            }
+        } .sheet(isPresented: $isPresented, content: {
+            AddBudgetScreen()
+        } )
+        .sheet(isPresented: $isFilterPresented, content: {
+            NavigationStack {
+                FilterScreen()
+            }
+        }
+        )
+    }
 }
 
 #Preview {
     NavigationStack {
         BudgetListScreen()
-            }            .environment(\.managedObjectContext, CoreDataProvider.preview.context)
+    }            .environment(\.managedObjectContext, CoreDataProvider.preview.context)
 }
 
 
